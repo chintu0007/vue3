6eb56071be59567f1cd30ede51300164aa73f819
Gaurang Parekh <gaurang.parekh@thegatewaycorp.com>
2022-08-22 18:48:41 +0530

    computed property vs methods

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
-const app = Vue.createApp({
-    data() {
-        return  {
-            counter:0
-        }
-    },
-    methods: {
-        increment() {
-            this.counter++;
-        }
+let taxRate = 1.23;
+const vm = Vue.createApp({
+  data() {
+    return {
+      price: 4,
+      items: 0,
+    };
+  },
+  methods: {
+    includingVatM() {
+        console.log('methods - includingVatM');
+        return (this.price * taxRate).toFixed(2);  
     }
-});
-
-const vm = app.mount('#app');
+  },
+  computed: {
+    taxRate() {
+      return (taxRate*100)-100;
+    },
+    includingVat() {
+        console.log('computed - includingVat');
+      return (this.price * taxRate).toFixed(2);
+    },
+    total() {
+      return (this.includingVat * this.items).toFixed(2);
+    },
+  },
+}).mount("#app");
