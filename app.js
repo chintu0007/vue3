dd7d95dc94ebcb1e3101cdfccdf868139f5523fc
Gaurang Parekh <gaurang.parekh@thegatewaycorp.com>
2022-08-23 16:06:51 +0530

    condtional rendering

--- app.js
+++ app.js
@@ -1,37 +1,30 @@
-const vm = Vue.createApp({
-  data() {
-    return {
-      name: "",
-      gender: "unknown",
-    };
-  },
-  computed: {
-    message() {
-      //return "Hello "+ this.name;
-      return `Hello ${this.prefix} ${this.name}`;
-    },
-    prefix() {
-      switch (this.gender) {
-        case "male":
-          return "Mr ";
-        case "female":
-          return "Ms";
-        default:
-          return "";
+const icons = {
+    rocket: '🚀',
+    meat: '🥩',
+    fish: '🐟',
+    us: '🇺🇸',
+    india: '🇮🇳',
+    vege: '🌱'
+  };
+  
+  const vm = Vue.createApp({
+    data() {
+      return {
+        started: false,
+        vegetarian: false,
+        realvegetarian: true,
+        basket: [
+          'Milk',
+          'Cheese',
+          'Carrot',
+          'Fish'
+        ]
       }
     },
-  },
-  watch: {
-    name(newName, oldName) {
-      console.log(newName);
-      console.log(oldName);
-
-      fetch(`https://api.genderize.io?name=${newName}`)
-        .then((response) => response.json())
-        .then((json) => (this.gender = json.gender));
-    },
-    message() {
-      console.log("The watcher was called");
-    },
-  },
-}).mount("#app");
+    computed: {
+      icons() { return icons },
+      isReallyVegetarian() {
+        return !this.basket.includes('Fish');
+      },
+    }
+  }).mount('#app');
