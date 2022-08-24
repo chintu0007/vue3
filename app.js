8e22557b5a11e40d4501481ee098eb77d9f9251a
Gaurang Parekh <gaurang.parekh@thegatewaycorp.com>
2022-08-24 21:47:46 +0530

    component prop attributes

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const app = Vue.createApp({
     data() {
-      return {}
+      return {
+        //
+      }
     },
     methods: {},
     computed: {}
@@ -8,7 +10,26 @@ const app = Vue.createApp({
 
 
   app.component('todo-list-item', {
-        props: ['task'],
+        props: {
+            task: {
+                type:String,
+                required:true,
+                // validator(value) {
+                //     return "Okay" === value;
+                // }
+            },
+            id:{
+                type:Number,
+                required:true,
+                validator(value) {
+                    return value >=1 && value <=100;
+                },
+                // default() {
+                    // return [];
+                // },
+            },
+
+        },
         template: `<div class="bg-white shadow-sm rounded-md text-gray-700 text-xs md:text-sm p-4">{{task}}</div>`
   });
   
