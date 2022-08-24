d4074d580f14a85c49259476f1f5ce64a21cee8e
Gaurang Parekh <gaurang.parekh@thegatewaycorp.com>
2022-08-24 15:46:18 +0530

    v-for fitlering and sorting

--- app.js
+++ app.js
@@ -1,26 +1,65 @@
 const vm = Vue.createApp({
-    data() {
-      return {
-        items: [
-          {
-            name: "Milk",
-            price: 1.5,
-          },
-          {
-            name: "Beer",
-            price: 3.1,
-          },
-          {
-            name: "Juice",
-            price: 2.5,
-          },
-        ],
-        countries: {
-          us: "United States",
-          de: "Germany",
-          br: "Brazil",
-          in: "India",
+  data() {
+    return {
+      todos: [
+        {
+          name: "Buy groceries",
+          done: false,
         },
-      };
-    }
-  }).mount('#app');
+        {
+          name: "Pay bills",
+          done: true,
+        },
+        {
+          name: "Take dogs for a walk",
+          done: true,
+        },
+      ],
+      showTodos: true,
+      sortOrder: "ASC",
+      onlyPaid: false,
+      invoices: [
+        {
+          id: 2,
+          item: "Udemy Course",
+          price: 9.99,
+          paid: false,
+        },
+        {
+          id: 1,
+          item: "Book",
+          price: 39.99,
+          paid: true,
+        },
+        {
+          id: 3,
+          item: "MacBook Air",
+          price: 599.99,
+          paid: true,
+        },
+      ],
+    };
+  },
+  computed: {
+    doneTodos() {
+      // TO BE IMPLEMENTED
+      return this.todos.filter((todo) => todo.done);
+    },
+    paidInvoices() {
+      // TO BE IMPLEMENTED
+
+      return this.invoices.filter((invoice) => invoice.paid);
+    },
+    sortedInvoices() {
+      // TO BE IMPLEMENTED
+      const invoices = this.onlyPaid ? this.paidInvoices : this.invoices;
+      return invoices.sort(
+            (a, b) => this.sortOrder=== 'ASC' ? a.id - b.id : b.id - a.id);
+    },
+  },
+  methods: {
+    toggleSortOrder() {
+      this.sortOrder = (this.sortOrder === "ASC") ? "DESC" : "ASC";
+    },
+  },
+}).mount("#app");
