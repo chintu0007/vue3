eb5de178996405a5baf2d2c1b8682b395a323c6f
Gaurang Parekh <gaurang.parekh@thegatewaycorp.com>
2022-08-24 12:19:24 +0530

    list rendering

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
 const vm = Vue.createApp({
     data() {
       return {
-        visible: true
-      }
-    },
-    methods: {
-      log(msg) {
-        console.log(msg);
-      },
-      hide() {
-        this.visible = false;
+        items: [
+            {
+              name: 'Milk',
+              price: 1.5
+            },
+            {
+              name: 'Beer',
+              price: 3.1
+            },
+            {
+              name: 'Juice',
+              price: 2.5
+            }
+          ],
+          countries: {
+            us:'United States',
+            de:'Germany',
+            br:'Brazil',
+            in:'India'
+          }
       }
     }
   }).mount('#app');
