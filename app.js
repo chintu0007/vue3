41f9f7d5c1dd84349f8cdd43ebc5fc67775a36de
Gaurang Parekh <gaurang.parekh@thegatewaycorp.com>
2022-08-25 11:35:35 +0530

    custom component events and listening to custom component events

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
+let nextTaskId = 100;
 const app = Vue.createApp({
     data() {
       return {
-        //
+        tasks:[]
       }
     },
-    methods: {},
+    methods: {
+        taskAdded(task) {
+            this.tasks.push({
+                id:nextTaskId++,
+                description:task,
+                done:false,
+                priority:false
+            });
+
+        }
+    },
     computed: {}
   });
 
@@ -18,19 +29,40 @@ const app = Vue.createApp({
                 //     return "Okay" === value;
                 // }
             },
-            id:{
-                type:Number,
-                required:true,
-                validator(value) {
-                    return value >=1 && value <=100;
-                },
-                // default() {
-                    // return [];
-                // },
-            },
+            // id:{
+            //     type:Number,
+            //     required:true,
+            //     validator(value) {
+            //         return value >=1 && value <=100;
+            //     },
+            //     // default() {
+            //         // return [];
+            //     // },
+            // },
 
         },
         template: `<div class="bg-white shadow-sm rounded-md text-gray-700 text-xs md:text-sm p-4">{{task}}</div>`
+  });
+
+  app.component('add-task-input', {
+        emits:['added'],    
+        data() {
+            return  {
+                task: ''
+            }
+        },
+        methods: {
+            add() {
+                this.$emit('added', this.task);
+                this.task='';
+            }
+        },
+        template:`<input type="text"
+        placeholder="Enter task and hit enter"
+        @keyup.enter="add" 
+        v-model="task"
+        class="block w-full rounded-sm text-lg p-4" />`,
+
   });
   
   app.mount('#app');
