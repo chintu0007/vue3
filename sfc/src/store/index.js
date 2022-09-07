80409c9bceabf6d9ac922b9434c33c607089a497
Gaurang Parekh <gaurang.parekh@thegatewaycorp.com>
2022-09-07 18:02:30 +0530

    using v-model with computed property getter-setter1

---
+++ sfc/src/store/index.js
@@ -0,0 +1,56 @@
+import { createStore } from "vuex";
+
+const store = createStore({
+  state() {
+
+    return {
+      onlyPending: false,
+      tasks: [
+        {
+          id: 1,
+          description: "Buy food for the dog",
+          priority: false,
+          done: false,
+        },
+        {
+          id: 2,
+          description: "Pay the bills",
+          priority: true,
+          done: false,
+        },
+        {
+          id: 3,
+          description: "Buy some computer games",
+          priority: false,
+          done: false,
+        },
+        {
+          id: 4,
+          description: "Go to the gym",
+          priority: false,
+          done: false,
+        },
+      ],
+    };
+  },
+  getters: {},
+  mutations: {
+    addTask(state, payload) {
+      state.tasks.push(payload);
+    },
+    updateTask(state, payload) {
+      const taskIndex = state.tasks?.findIndex(
+        (task) => task.id === payload.id
+      );
+
+      if (taskIndex !== undefined && taskIndex !== -1) {
+        state.tasks[taskIndex] = payload;
+      }
+    },
+    setOnlyPending(state, payload) {
+      state.onlyPending = payload;
+    },
+  },
+});
+
+export default store;
