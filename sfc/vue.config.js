176a064fe02197686cb376a7d9133636cc7bbe05
Gaurang Parekh <gaurang.parekh@thegatewaycorp.com>
2022-09-07 12:27:50 +0530

    components and single file components

--- sfc/vue.config.js
+++ sfc/vue.config.js
@@ -1,4 +1,15 @@
-const { defineConfig } = require("@vue/cli-service");
-module.exports = defineConfig({
-  transpileDependencies: true,
-});
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/app'
+    : '',
+  pages: {
+    index: {
+      title: 'Exploring the world of VueJS',
+      entry: './src/main.js'
+    },
+    landing: {
+      title: 'Landing Page',
+      entry: './src/landing.js'
+    }
+  }
+}
